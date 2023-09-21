f49d1249bc8e83ef3500e9cc41af6beeabbcc537
koheinthurako <captainthura99@gmail.com>
2023-09-21 23:17:49 +0630

    add module

---
+++ jsRefresh/module.js
@@ -0,0 +1,5 @@
+import mySelf from "./module2.js";
+// module နဲ့ code ခွဲရေးခြင်းက Maintain လုပ်ရလွယ်ကူတယ်
+
+// module2 ထဲက export default id; ဆိုတာ default ပါလာရင် id ထဲက data အကုန်လုံးကို ယူလာပြီး ဒီဘက် file မှာ id အစား ကြိုက်တဲ့နာမည်ပေးလို့ရတယ်
+console.log(mySelf.name, mySelf.age, mySelf.gender);
---
+++ jsRefresh/module2.js
@@ -0,0 +1,12 @@
+export const id = {
+    name: "TZH",
+    age: 24,
+    gender: "Male"
+}
+
+// export function run() {
+//     const {name, age, gender} = id;
+//     return {name, age, gender};
+// }
+
+export default id;
