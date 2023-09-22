715541acf6431d310d6a6090a664d15f4da8b93a
koheinthurako <captainthura99@gmail.com>
2023-09-22 22:01:55 +0630

    finish ternaryOperator texts

--- jsRefresh/ternaryOperator.js
+++ jsRefresh/ternaryOperator.js
@@ -7,3 +7,21 @@ const x = 3;
 //     console.log("I am not 3");
 // }
 
+// Ternary Operator Syntax use in JSX code
+// x === 3 ? console.log("I am 3") : console.log("I am not 3");
+
+
+
+// ရိုးရိုး if...else မှာ အမှန်ပဲလိုချင်ရင် ဒီလိုစစ်လို့ရပေမယ့်
+// if(x === 3) {
+//     console.log("I am 3");
+// }
+
+// Ternary Operator JSX မှာတော့ အမှန်ပဲလိုချင်ရင် အမှားနေရာမှာ null ထည့်ပေးရတယ်
+// x === 3 ? console.log("I am 3") : null;
+
+// အကယ်၍ အမှန်ချည်းပဲလိုချင်လို့ ရိုးရိုး if else လို JSX မှာ စစ်ချင်တယ်ဆိုရင်တော့
+// One Way Condition or Logical and Syntax ကို သုံးလို့ရတယ်
+x === 2 && console.log("I am 3");
+// ဒီလို logical syntax (&&) ကို သုံးပြီးရေးလိုက်တာက x === 2 က false ဖြစ်သွားတာနဲ့ နောက်က condition(console.log) ကို မစောင့်တော့ပဲ အလုပ်မလုပ်တော့တာပဲဖြစ်တယ်
+
