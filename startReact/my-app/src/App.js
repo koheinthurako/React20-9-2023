3a5e8a69e1349d33aeb3cd0031c0970c8bacab23
koheinthurako <captainthura99@gmail.com>
2023-09-25 22:59:23 +0630

    add notes

--- startReact/my-app/src/App.js
+++ startReact/my-app/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+// react တွင် JSX ကိုသုံးသည်
+// jsx = JavaScript Syntax Expression
+// react မှာ JavaScript reserved words တွေကို သုံးလို့မရတော့ပါ
+// ဥပမာ html ထဲက class ဆိုရင် jsx မှာ className ဖြစ်သွားတယ်
+// HTML subclosing tag တွေဖြစ်တဲ့ <br>, <hr>, <input> etc... တို့ကို jsx တွင်
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <label htmlFor='hello'>Hello</label>
+      <input type='text' name='hello' id='hello' />
     </div>
   );
 }
