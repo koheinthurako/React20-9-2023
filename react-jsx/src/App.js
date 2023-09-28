fc91132894f0b014c37cea3f64777ea028925e16
koheinthurako <captainthura99@gmail.com>
2023-09-28 22:11:22 +0630

    start basic knowledge

--- react-jsx/src/App.js
+++ react-jsx/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {Fragment} from "react";
 
 function App() {
   return (
+    // element ၁ခုထပ်ပိုပြီး return ပြန်ချင်တဲ့အခါ React ကို import လုပ်ပြီး React.Fragment ကိုသုံး
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <hr/>
+      <br/>
+      <input type='text' placeholder='write something...'/>
     </div>
+    <div></div>
+    </>
   );
 }
 
