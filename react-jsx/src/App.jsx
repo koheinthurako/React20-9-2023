c21da3986561224cf69b0ccba7fa430036784249
koheinthurako <captainthura99@gmail.com>
2023-09-30 23:08:02 +0630

    startPoint

--- react-jsx/src/App.jsx
+++ react-jsx/src/App.jsx
@@ -1,9 +1,15 @@
+import "../src/App.css";
 import One from "./components/One";
+import Two from "./components/Two";
 
 const App = () => {
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <One/>
+      <Two/>
+    </div>
+  );
 };
 
 export default App;
---
+++ react-jsx/src/components/Two.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Two = () => {
+  return (
+    <div>
+        <p>I am component two</p>
+    </div>
+  )
+}
+
+export default Two;
