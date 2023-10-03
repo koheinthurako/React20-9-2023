457ad02af2d36c39194c5cc65d182e3090fe141d
koheinthurako <captainthura99@gmail.com>
2023-10-03 23:06:07 +0630

    add propslist data 3

--- react-jsx/src/App.jsx
+++ react-jsx/src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
 
   const data = ["Ko Hein", "Thet Zaw Hein", "Captain"];
 
-
   return (
     <div className="container">
       <Nav/>
@@ -20,13 +19,14 @@ const App = () => {
       {/* 
         Props အကြောင်းအစ 
         အပေါ်က data array ထဲက data တွေကို အောက်က propslist ထဲ data တွေ လှမ်းပေးချင်ရင် props အနေနဲ့ပေးလို့ရတယ်
-        Props ဆိုတာက HTML မှာ Attribute လို့ခေါ်တယ် rendering လုပ်တော့ propslist ကို လှမ်းခေါ်တဲ့အခါ Propslist.jsx
+        Props ဆိုတာက HTML မှာ Attribute လို့ခေါ်တယ် Parent Level ဖြစ်တဲ့ App ကနေ child level ဖြစ်တဲ့ Propslist ထဲကို
+        data passing ပေးချင်ရင် props နဲ့ passing ပေးတယ် rendering လုပ်တော့ propslist ကို လှမ်းခေါ်တဲ့အခါ Propslist.jsx
         ထဲက arrow function က အလုပ်လုပ်သွားပြီး props ဖြစ်တဲ့ name ကို ဟိုဘက်က function parameter ထဲ passing
         ပေးလိုက်ပြီး object တွေလို data array ထဲက တန်ဖိုးတွေကို ပြန်ခေါ်သုံးလို့ရသွားတယ်
       */}
-      <Propslist name={data[0]} age={24}/>
-      <Propslist name={data[1]} age={24}/>
-      <Propslist name={data[2]} age={24}/>
+      <Propslist name={data[0]} age={24} isDone/>
+      <Propslist name={data[1]} age={24} isDone={false}/>
+      <Propslist name={data[2]} age={24} isDone/>
 
       <Footer/>
 
--- react-jsx/src/components/Propslist.jsx
+++ react-jsx/src/components/Propslist.jsx
@@ -1,9 +1,17 @@
 import "./Propslist.css"
 
-const Propslist = ({name, age}) => {
+const Propslist = ({name, age, isDone}) => {
+
+    const style = {
+        bgStyle: {
+            background: "black",
+            textDecoration: "line-through"
+        }
+    }
+
   return (
-    <div className="listContainer">
-        <h3>This is Propslist named "{name}" and age is {age}</h3>
+    <div className="listContainer" style={isDone ? style.bgStyle : null}>
+        <h3>This is Propslist named "{name}" and age is {age}.</h3>
     </div>
   )
 }
