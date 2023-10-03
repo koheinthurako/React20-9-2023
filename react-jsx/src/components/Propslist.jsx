b6aee7069fbdff6148d732aa2ac7452408c9c40d
koheinthurako <captainthura99@gmail.com>
2023-10-03 23:15:21 +0630

    add style binding & class binding

--- react-jsx/src/components/Propslist.jsx
+++ react-jsx/src/components/Propslist.jsx
@@ -2,15 +2,22 @@ import "./Propslist.css"
 
 const Propslist = ({name, age, isDone}) => {
 
-    const style = {
-        bgStyle: {
-            background: "black",
-            textDecoration: "line-through"
-        }
-    }
+    // const style = {
+    //     bgStyle: {
+    //         background: "black",
+    //         textDecoration: "line-through"
+    //     }
+    // }
 
   return (
-    <div className="listContainer" style={isDone ? style.bgStyle : null}>
+    // ဒါက style binding ဖြစ်ပြီး အဲ့ဒါကိုမသုံးပဲ class binding ကိုလည်းသုံးလို့ရတယ်
+    // <div className="listContainer" style={isDone ? style.bgStyle : null}>
+    //     <h3>This is Propslist named "{name}" and age is {age}.</h3>
+    // </div>
+
+    // ဒါက class binding ဖြစ်ပြီး အပေါ်က style variable ထဲကမဟုတ်ပဲ Propslist.css ထဲက style class ကိုယူသုံးထားတယ်
+    // အောက်က className ထဲမှာ template literal ကိုအသုံးပြုထားပြီး js တုန်းကလို တန်းရေးလို့မရပဲ {``} ထဲမှာပဲ ရေးလို့ရတယ်
+    <div className={`listContainer ${isDone && "done"}`}>
         <h3>This is Propslist named "{name}" and age is {age}.</h3>
     </div>
   )
