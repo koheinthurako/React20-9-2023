1ce0ec1a27f9ce62713791362213b4c716f8441a
koheinthurako <captainthura99@gmail.com>
2023-10-09 23:21:45 +0630

    add add and remove functions

--- hook/src/components/Counter.jsx
+++ hook/src/components/Counter.jsx
@@ -1,20 +1,40 @@
 import {useState} from "react";
 import "./Counter.css";
 
-// State တစ်ခု change တိုင်း Rerender ပြန်လုပ်တယ်
+// State တစ်ခု change တိုင်း components တွေကို Rerender ပြန်လုပ်တယ် - အရေးကြီးတယ်
 
 const Counter = () => {
     const [count, setCount] = useState(0);
 
     const handleAdd = () => {
-        setCount(count + 1);
+        // အောက်က unsafe state change ဖြစ်တယ် ဘာလို့လဲဆိုတော့ looping ပတ်ပြီး setCount မှာ count ကို ၁ တိုးပေမယ့်
+        // console.log(count) နေရာတော့ useState(0) အထဲက 0 ကိုပဲသွားပြီး console ထုတ်ပြနေတယ် ဒါကြောင့် unsafe state ဖြစ်တယ်
+        // for(let i = 0; i < 10; i++) {
+        //     console.log(count);
+        //     setCount(count + 1);
+        // }
+
+        // အောက်က safe state change ဖြစ်တယ် ဘာလို့လဲဆိုတော့ looping ပတ်ပြီး setCount ထဲက preCount ဆိုတာက အပေါ်ကလိုမဟုတ်ပဲ
+        // loop တစ်ခါပတ်တိုင်း const preCount = useState(count) ပုံစံဖြစ်သွားပြီး useState ထဲက တန်ဖိုးက loop အလိုက်ပြောင်းလဲနေသည့်အတွက်
+        // console.log(preCount) သည် loop ပတ်တိုင်းပြောင်းလဲပြီး မတူညီတဲ့ တန်ဖိုးများထွက်လာသည့်အတွက် safe state ဖြစ်တယ်
+        // for(let i = 0; i < 10; i++) {
+        //     setCount((preCount) => {
+        //         // ဒီက preCount ဆိုတာ useState ထဲက count တန်ဖိုးကိုပဲကိုယ်စားပြုသည်
+        //         console.log(preCount);
+        //         return preCount + 1;
+        //     });
+        // }
+
+        // Shorten syntax
+        setCount(pre => pre + 1);
+        
     };
 
     const handleRemove = () => {
-
+        setCount(pre => pre - 1);
     };
 
-    console.log(count);
+    // console.log(count);
 
   return (
     <div className="contentBox">
