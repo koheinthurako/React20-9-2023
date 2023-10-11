6db1b886996adccaa764dd62c00abb566f21fc97
koheinthurako <captainthura99@gmail.com>
2023-10-11 23:18:48 +0630

    note more about useEffect

--- hook/src/components/Effect/ComponentEffect.jsx
+++ hook/src/components/Effect/ComponentEffect.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // useEffect အစ
 // componentDidMount, componentDidUpdate, and componentWillUnmount
-// Mounted ဆိုတာ virtual DOM ကနေ screen ပေါ်ကို ရောက်လာတဲ့အချိန်ကို Mounted ဖြစ်တယ်လို့ခေါ်တယ်
+// Mounted ဆိုတာ virtual DOM ကနေ real screen ပေါ်ကို ရောက်လာတဲ့အချိန်ကို Mounted ဖြစ်တယ်လို့ခေါ်တယ်
 
 /* componentDidMount */
-// virtual DOM ကနေ screen ပေါ်ကိုရောက်လာတာနဲ့ componentDidMount ကအလုပ်လုပ်တယ်
+// virtual DOM ကနေ real screen ပေါ်ကိုရောက်လာတာနဲ့ componentDidMount ကအလုပ်လုပ်တယ်
 
 /* componentDidUpdate */
 // ချိတ်ဆက်ထားတဲ့ components ထဲက state ပဲဖြစ်ဖြစ်ဘာဖြစ်ဖြစ် တစ်ခုခုပြောင်းလဲသွားတိုင်း componentDidUpdate method ကအလုပ်လုပ်တယ်
@@ -17,12 +17,40 @@ import { useEffect } from "react";
 
 const ComponentEffect = () => {
 
+    const [count, setCount] = useState(0);
+    const [toggle, setToggle] = useState(true);
+
+    // mounted => ComponentsMounted
     useEffect(() => {
-        console.log("Use Effect start");
+        console.log("Mounted");
+        // အဆုံးမှာ ဒီလို [] ထည့်ပေးလိုက်ခြင်းဖြင့် mounted ဖြစ်တဲ့အချိန်မှာပဲအလုပ်လုပ်စေတယ်
+    }, [])
+
+    // didComponents
+    useEffect(() => {
+        console.log("I am didComponents", count);
     })
+
+    // components will update when value was change
+    useEffect(() => {
+        console.log("toggle was changed");
+    }, [toggle]);
     
+    const handleClick = () => {
+        setCount(pre => pre + 1);
+    }
+
+    const handleToggle = () => {
+        setToggle(pre => !pre);
+    }
+
   return (
-    <div>ComponentEffect</div>
+    <div>
+        <p>{count}</p>
+        {toggle && <h1>Hello</h1>}
+        <button onClick={handleClick}>Add</button>
+        <button onClick={handleToggle}>Toggle</button>
+    </div>
   )
 }
 
