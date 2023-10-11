3d9dddf0e249bc84f71f7e73a22d43a71f7f8320
koheinthurako <captainthura99@gmail.com>
2023-10-11 22:37:44 +0630

    add change color mini program test

--- hook/src/components/ComponentsOne.jsx
+++ hook/src/components/ComponentsOne.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import "./One.css";
 
 const ComponentsOne = () => {
 
-    const [toogle, setToogle] = useState();
+    const [toogle, setToogle] = useState(true);
+
+    const handleClick = () => {
+        // pre သည် ဒီနေရာမှာ toogle နေရာကိုကိုယ်စားပြုပြီး useState(ထဲကတန်ဖိုး) ကို သွားယူသည့်အတွက် return နေရာတွင် !pre ကိုပြန်ပြီး toogle ဖြစ်နေသည်
+        setToogle((pre) => !pre);
+    }
     
   return (
-    <div>ComponentsOne</div>
+    <div>
+        {/* အောက်က ဟာက Conditional Styling ဖြစ်တယ် className ထဲမှာ condition စစ်ပြီးတော့ မှန်တဲ့ class ကို ထည့်ခြင်း */}
+        <h1 className={toogle ? 'introText' : 'changeTextColor'}>Hello World</h1>
+        <button onClick={handleClick}>Toggle</button>
+    </div>
   )
 }
 
