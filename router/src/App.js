3e35757d4c5bd25a87adae288fa2c4179f9e40f8
koheinthurako <captainthura99@gmail.com>
2023-10-12 23:07:06 +0630

    add react-router-dom

--- router/src/App.js
+++ router/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Service from "./pages/Service";
 
 const App = () => {
   return (
