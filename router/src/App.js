2b26d95429df120a021eba3d4b96f04b69607acf
koheinthurako <captainthura99@gmail.com>
2023-10-13 22:35:06 +0630

    connect route

--- router/src/App.js
+++ router/src/App.js
@@ -2,10 +2,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Service from "./pages/Service";
+import { BrowserRouter as Rounter, Routes, Route, Router} from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
+    // <Router></Router> ဆိုပြီးအရင်ရေးခြင်းသည် Rounter တွေစုထားတဲ့နေရာကိုဆိုလိုသည် အထဲမှ Routes ကတော့ Route တစ်ခုချင်းစီထည့်ရေးရတဲ့ parent
+    <Rounter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/service" element={<Service/>}/>
+      </Routes>
+    </Rounter>
   )
 }
 
--- router/src/index.js
+++ router/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Rounter, Routes, Route} from "react-router-dom";
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <App/>
 );
 
