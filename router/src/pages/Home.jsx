e62ce288d4c523ef43f3587c55b8a244c0186919
koheinthurako <captainthura99@gmail.com>
2023-10-15 14:48:52 +0630

    start using useNavigate()

--- router/src/pages/Home.jsx
+++ router/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import "./page.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+  // useNavigate ဆိုတဲ့ react-router-dom မှ support feature ကိုအသုံးပြုပြီး button တစ်ခုခုကိုနှိပ်လိုက်တာနဲ့ ဘယ်သွားမလဲဆိုတာကို အောက်ကလိုပြန်သုံးနိုင်တယ်
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/service");
+  }
+
   return (
     <div className="container home">
       <h1>Home</h1>
-      <button>Go to Service</button>
+      <button onClick={handleClick}>Go to Service</button>
     </div>
   )
 }
