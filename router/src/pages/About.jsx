3ce197b179048c6fc0d9303ac5193f58ff3f73e0
koheinthurako <captainthura99@gmail.com>
2023-10-20 23:21:17 +0630

    test form using useSearchParams

--- router/src/pages/About.jsx
+++ router/src/pages/About.jsx
@@ -1,5 +1,6 @@
 import "./page.css";
-import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const About = () => {
 
@@ -9,11 +10,30 @@ const About = () => {
     navigate("/");
   }
 
+  // useSearchParams ကိုသုံးပြီး url တစ်ခုမှာ query string အနေနဲ့ ရှာလိုက်တဲ့ data တွေကို api မှတစ်ဆင့် server ကို fetch လို့ရတယ်
+  const [query, setQuery] = useSearchParams();
+
+  const formRef = useRef();
+
+  const handleClick = e => {
+    // console.log("Click");
+    e.preventDefault();
+    // setQuery({name: "Thet Zaw Hein", age:25});
+    console.log(formRef.current[0].value);
+    console.log(formRef.current[1].value);
+  }
+
+
   return (
     <div className="container about">
       <div className="toBoxes">
-        <h1>Back To Home</h1>
-        <button onClick={toHome}>Click Here!</button>
+        {/* <h1>Back To Home</h1> */}
+        {/* <button onClick={toHome}>Click Here!</button> */}
+        <form ref={formRef}>
+          <input type="text" placeholder="Username" style={{width:"320px", height: "34px", borderRadius: "7px", border: "none", paddingLeft: "12px"}}/>
+          <input type="number" placeholder="Password" style={{width:"320px", height: "34px", borderRadius: "7px", border: "none", paddingLeft: "12px", margin: "20px 0"}}/>
+          <button onClick={handleClick}>Search Query</button>
+        </form>
       </div>
     </div>
   )
