69489429253ae1be37bb6512c3f7164927392b61
koheinthurako <captainthura99@gmail.com>
2023-10-22 22:57:09 +0630

    start using tailwindcss

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  // အောက်က "src" တွင် ** တွေသည် src file ထဲတွင် file တွေဘယ်လောက်ခွဲခွဲရပြီး အထဲမှာ {js,jsx,ts,tsx} တွေပါရင်လည်းရသည်
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
