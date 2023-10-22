43b36bc7c685dbb4b7eed0398eaed3fa6a8f6864
koheinthurako <captainthura99@gmail.com>
2023-10-22 23:18:30 +0630

    start note about tailwindcss

--- tailwindcss/src/App.js
+++ tailwindcss/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
       {/* 
         tailwind မှာ h1,h2,p ကွဲပေမယ့် index.css ထဲက @tailwind base ဆိုတဲ့ အပေါ်မူတည်ပြီး အလုပ်အရင်လုပ်တယ်
         အဲ့တာကြောင့် elements တွေအကုန်လုံးကို တညီတညွတ်တည်း p အနေနဲ့ပဲ default ချပြပါတယ်
-        ပြီးမှ ကိုယ်ကြိုက်သလိုပြန်ပြင်ရပါတယ်
+        ပြီးမှ ကိုယ်ကြိုက်သလိုပြန်ပြင်ရပါတယ် custom css တွေရေးချင်ရင် postcss preprocessor ကို install လုပ်ပြီး သုံးရတယ်
       */}
       <h1 className='text-red-500'>Hello World</h1>
       <h2 className='ms-12 text-blue-600'>Hello World</h2>
