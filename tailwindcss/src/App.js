89385745913ae7123a82bb24935c8a093a7ca1fd
koheinthurako <captainthura99@gmail.com>
2023-10-22 23:16:53 +0630

    install postcss

--- tailwindcss/src/App.js
+++ tailwindcss/src/App.js
@@ -3,7 +3,19 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      {/* 
+        tailwind မှာ h1,h2,p ကွဲပေမယ့် index.css ထဲက @tailwind base ဆိုတဲ့ အပေါ်မူတည်ပြီး အလုပ်အရင်လုပ်တယ်
+        အဲ့တာကြောင့် elements တွေအကုန်လုံးကို တညီတညွတ်တည်း p အနေနဲ့ပဲ default ချပြပါတယ်
+        ပြီးမှ ကိုယ်ကြိုက်သလိုပြန်ပြင်ရပါတယ်
+      */}
       <h1 className='text-red-500'>Hello World</h1>
+      <h2 className='ms-12 text-blue-600'>Hello World</h2>
+      <p className='text-red-500'>Hello World</p>
+      <ul>
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+      </ul>
     </div>
   )
 }
