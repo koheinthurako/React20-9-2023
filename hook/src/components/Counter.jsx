cf21774661a10dbad914b16e234b6a4bebdaaa1c
koheinthurako <captainthura99@gmail.com>
2025-01-10 14:04:13 +0630

    add more notes about safe and unsafe changes

--- hook/src/components/Counter.jsx
+++ hook/src/components/Counter.jsx
@@ -9,10 +9,10 @@ import "./Counter.css";
 
 const Counter = ({data}) => {
     const [count, setCount] = useState(0);
-    // count က useState() ထဲမှာကိုယ်စားပြုသည့် '0' ကို ကိုယ်စားပြုသည်
+    // count က useState() ထဲမှ '0' ကို ကိုယ်စားပြုသည်
 
     const handleAdd = (x) => {
-        // အောက်က unsafe state change ဖြစ်တယ် ဘာလို့လဲဆိုတော့ looping ပတ်ပြီး setCount မှာ count ကို ၁ တိုးပေမယ့်
+        // အောက်က unsafe state change လို့ခေါ်တယ်
         // console.log(count) နေရာတော့ useState(0) အထဲက 0 ကိုပဲသွားပြီး console ထုတ်ပြနေတယ် ဒါကြောင့် unsafe state ဖြစ်တယ်
         // for(let i = 0; i < 10; i++) {
         //     console.log(count);
@@ -22,6 +22,7 @@ const Counter = ({data}) => {
         // အောက်က safe state change ဖြစ်တယ် ဘာလို့လဲဆိုတော့ looping ပတ်ပြီး setCount ထဲက preCount ဆိုတာက အပေါ်ကလိုမဟုတ်ပဲ
         // loop တစ်ခါပတ်တိုင်း const preCount = useState(count) ပုံစံဖြစ်သွားပြီး useState ထဲက တန်ဖိုးက loop အလိုက်ပြောင်းလဲနေသည့်အတွက်
         // console.log(preCount) သည် loop ပတ်တိုင်းပြောင်းလဲပြီး မတူညီတဲ့ တန်ဖိုးများထွက်လာသည့်အတွက် safe state ဖြစ်တယ်
+        // အဓိကကတော့ Loop ပတ်ပြီး setCount() ကိုခေါ်တာတူပေမယ့် Callback Function တစ်ခုထည့်လိုက်တာနဲ့ ကွာခြားသွားခြင်းဖြစ်တယ်
         // for(let i = 0; i < 10; i++) {
         //     setCount((preCount) => {
         //         // ဒီက preCount ဆိုတာ useState ထဲက count တန်ဖိုးကိုပဲကိုယ်စားပြုသည်
@@ -41,20 +42,20 @@ const Counter = ({data}) => {
 
     console.log("Child rendering");
 
-  return (
-    <div className="contentBox">
-        <p>{data}</p>
-        <p>{count}</p>
-        <div className="btnBox">
-            <button onClick={() => {
-                handleAdd(2);
-            }}>Add</button>
-            <button onClick={() => {
-                handleRemove(2);
-            }}>Remove</button>
+    return (
+        <div className="contentBox">
+            <p>{data}</p>
+            <p>{count}</p>
+            <div className="btnBox">
+                <button onClick={() => {
+                    handleAdd(2);
+                }}>Add</button>
+                <button onClick={() => {
+                    handleRemove(2);
+                }}>Remove</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 // export default Counter;
