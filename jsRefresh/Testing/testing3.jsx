c4236dab3f3d9fa7a8dfe3fba906cfdddf1a4bd2
koheinthurako <captainthura99@gmail.com>
2025-05-07 01:09:16 +0700

    continue testing jsx acoumd

---
+++ jsRefresh/Testing/testing3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Testing = () => {
+  return (
+    <>
+        <div>Testing 3</div>
+        <div>
+            <h1>Header</h1>
+        </div>
+    </>
+  )
+}
