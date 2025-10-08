2615b8588662c3045f5043a40635801c0e188e1c
koheinthurako <captainthura99@gmail.com>
2025-10-08 18:26:26 +0700

    add more notes

--- jsRefresh/2_module.js
+++ jsRefresh/2_module.js
@@ -1,4 +1,5 @@
 // module နဲ့ code ခွဲရေးခြင်းက Maintain လုပ်ရလွယ်ကူတယ်
+// Export တွေကိုတော့ သေချာမှတ်ထားပေးပါ
 
 // import mySelf from "./module2.js";
 // module2 ထဲက export default id; ဆိုတာ default ပါလာရင် id ထဲက data အကုန်လုံးကို ယူလာပြီး ဒီဘက် file မှာ id အစား ကြိုက်တဲ့နာမည်ပေးလို့ရတယ်
